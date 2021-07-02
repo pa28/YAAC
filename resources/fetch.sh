5bb54c79c5a4ff5c1a5dd6f13c010c2c0a3e9ec7
Richard Buckley <richard.buckley@ieee.org>
2021-07-02 11:09:52 -0400

    Update gitignore.

--- resources/fetch.sh
+++ resources/fetch.sh
@@ -4,11 +4,6 @@ echo "Fetching YAAC.zip from https://www.ka2ddo.org/ka2ddo/YAAC.zip"
 
 mkdir -p resources/YAAC
 
-cat << END > YAAC.sh
-#!/bin/bash
-/usr/bin/java -jar /usr/local/share/YAAC/YAAC.jar
-END
-
 if [ -f resources/YAAC.zip ]; then
   rm resources/YAAC.zip
   rm -rf resources/YAAC/*
