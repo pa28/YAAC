fb5191e2c72544d42c4ab0250bcec1557d00e9a6
Richard Buckley <richard.buckley@ieee.org>
2021-07-02 09:55:11 -0400

    Initial commit.

---
+++ resources/fetch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Fetch YAAC"
+
+cat << END > YAAC.sh
+#!/bin/bash
+/usr/bin/java -jar /usr/local/share/YAAC/YAAC.jar
+END
+
+if [ -f resources/YAAC.zip ]; then
+  rm resources/YAAC.zip
+  rm -rf resources/YAAC/*
+fi
+
+cd resources && wget --quiet https://www.ka2ddo.org/ka2ddo/YAAC.zip && cd YAAC && unzip ../YAAC.zip > /dev/null
